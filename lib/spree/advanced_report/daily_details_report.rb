ee403382e9d7e98d0a96a9676d5a57c2fbde17f4
Pete Campbell <pete@sumirolabs.com>
2013-06-28 00:46:26 -0400

    More updates to the detail form, changed it from shipment-based to payment-based

--- lib/spree/advanced_report/daily_details_report.rb
+++ lib/spree/advanced_report/daily_details_report.rb
@@ -1,5 +1,5 @@
 class Spree::AdvancedReport::DailyDetailsReport < Spree::AdvancedReport::IncrementReport
-  attr_accessor :date, :shipments, :adjustments
+  attr_accessor :date, :shipments, :adjustments, :orders, :payments
 
   def name
     "Detailed Units Sold"
@@ -16,6 +16,19 @@ class Spree::AdvancedReport::DailyDetailsReport < Spree::AdvancedReport::Increme
   def initialize(params)
     super(params)
 
+    results = products_for_date(date)
+    date = params[:date] ? Date.parse(params[:date]) : Date.today
+
+    self.date = date
+    self.data = results[:products]
+    self.shipments = results[:shipments]
+    self.adjustments = results[:adjustments]
+    self.orders = results[:orders]
+    self.payments = results[:payments]
+
+  end
+
+  def products_for_date2(date)
     date = params[:date] ? Date.parse(params[:date]) : Date.today
     date_hash = {created_at: date.beginning_of_day..date.end_of_day}
 
@@ -28,11 +41,52 @@ class Spree::AdvancedReport::DailyDetailsReport < Spree::AdvancedReport::Increme
       products[u.variant][:count] += 1
     end
 
-    self.shipments = units.collect{|u| u.shipment}.uniq
-    self.adjustments = Spree::Adjustment.where(date_hash).where("spree_adjustments.source_type != 'Spree::Shipment' OR spree_adjustments.source_type IS NULL")
+    shipments = units.collect{|u| u.shipment}.uniq
+    adjustments = Spree::Adjustment.where(date_hash).where("spree_adjustments.source_type != 'Spree::Shipment' OR spree_adjustments.source_type IS NULL")
 
-    self.date = date
-    self.data = products
-
+    [products, shipments, adjustments]
   end
+
+  def products_for_date(date)
+    date = params[:date] ? Date.parse(params[:date]) : Date.today
+    conditions = {created_at: date.beginning_of_day..date.end_of_day, state: 'completed'}
+
+    payments = Spree::Payment.joins(:order).where(conditions).where('spree_orders.completed_at is not null').includes(order: [:shipments, :adjustments])
+    orders = payments.collect{|p| p.order}.uniq
+    shipments = orders.collect(&:shipments).flatten
+    adjustments = orders.collect(&:adjustments).flatten.select{|a| a.source_type != 'Spree::Shipment'}
+
+    line_items = orders.collect(&:line_items).flatten
+
+    products = line_items.reduce({}) do |memo, item|
+      memo[item.variant] ||= {amount: 0, quantity: 0}
+      memo[item.variant][:amount] += item.amount
+      memo[item.variant][:quantity] += item.quantity
+      memo
+    end
+
+    {products: products, shipments: shipments, adjustments: adjustments, orders: orders, payments: payments}
+  end
+
+
+  # This is a placeholder for logic that determines how many items in an order were actually shipped.
+  # It may never be used again
+
+  def items_shipped_from_an_order
+    # For each order, determine how many 
+    items = {}
+    orders.each do |o|
+      shipped_variants = o.inventory_units.collect{|i| i.variant}
+      o.line_items.each do |i|
+        variant = i.variant
+        quantity = shipped_variants.count(variant)
+        items[variant] ||= {count: 0, price: 0}
+        items[variant][:count] += 1
+        items[variant][:price] += i.price * quantity
+      end
+    end
+
+    [items, shipments, adjustments]
+  end
+
 end
