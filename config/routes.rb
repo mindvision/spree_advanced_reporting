3ecd3e9d8299b5e85ba5e5d0d3d4df0a664fb5c4
Ben Radler <ben@benradler.com>
2013-09-29 18:26:19 -0700

    Rails 4 compatible routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
- Spree::Core::Engine.routes.prepend do
+Spree::Core::Engine.routes.draw do
   #namespace :admin do
   #  resources :reports, :only => [:index, :show] do
   #    collection do
@@ -6,37 +6,19 @@
   #    end
   #  end
   #end
-  match '/admin/reports/revenue' => 'admin/reports#revenue',  :via  => [:get, :post],
-                                                              :as   => 'revenue_admin_reports'
+  match '/admin/reports/revenue' => 'admin/reports#revenue', :via => [:get, :post], :as => 'revenue_admin_reports'
+  match '/admin/reports/count'   => 'admin/reports#count',   :via => [:get, :post], :as => 'count_admin_reports'
+  match '/admin/reports/units'   => 'admin/reports#units',   :via => [:get, :post], :as => 'units_admin_reports'
+  match '/admin/reports/profit'  => 'admin/reports#profit',  :via => [:get, :post], :as => 'profit_admin_reports'
 
-  match '/admin/reports/count' => 'admin/reports#count',  :via  => [:get, :post],
-                                                          :as   => 'count_admin_reports'
+  match '/admin/reports/top_customers' => 'admin/reports#top_customers', :via  => [:get, :post], :as => 'top_customers_admin_reports'
+  match '/admin/reports/top_products'  => 'admin/reports#top_products',  :via  => [:get, :post], :as => 'top_products_admin_reports'
 
-  match '/admin/reports/units' => 'admin/reports#units',  :via  => [:get, :post],
-                                                          :as   => 'units_admin_reports'
+  match '/admin/reports/geo_revenue' => 'admin/reports#geo_revenue', :via => [:get, :post], :as => 'geo_revenue_admin_reports'
+  match '/admin/reports/geo_units'   => 'admin/reports#geo_units',   :via => [:get, :post], :as => 'geo_units_admin_reports'
+  match '/admin/reports/geo_profit'  => 'admin/reports#geo_profit',  :via => [:get, :post], :as => 'geo_profit_admin_reports'
 
-  match '/admin/reports/profit' => 'admin/reports#profit',  :via => [:get, :post],
-                                                            :as  => 'profit_admin_reports'
-
-
-  match '/admin/reports/top_customers' => 'admin/reports#top_customers',  :via  => [:get, :post],
-                                                                          :as   => 'top_customers_admin_reports'
-
-  match '/admin/reports/top_products' => 'admin/reports#top_products',  :via  => [:get, :post],
-                                                                        :as   =>  'top_products_admin_reports'
-
-  match '/admin/reports/geo_revenue' => 'admin/reports#geo_revenue', :via => [:get, :post],
-                                                                      :as => 'geo_revenue_admin_reports'
-
-  match '/admin/reports/geo_units' => 'admin/reports#geo_units', :via => [:get, :post],
-                                                                  :as => 'geo_units_admin_reports'
-
-  match '/admin/reports/geo_profit' => 'admin/reports#geo_profit', :via => [:get, :post],
-                                                                    :as => 'geo_profit_admin_reports'
-
-  match '/admin/reports/daily_details' => 'admin/reports#daily_details',  :via  => [:get, :post],
-                                                          :as   => 'daily_details_admin_reports'
-
+  match '/admin/reports/daily_details' => 'admin/reports#daily_details', :via  => [:get, :post], :as   => 'daily_details_admin_reports'
 
   # match "/admin" => "admin/advanced_report_overview#index", :as => :admin
 end
