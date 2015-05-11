89656c67a7a3ab099143a9496e1a823af4bb14fd
Joseph Mullins <joseph@mindvision.com.au>
2015-05-11 12:40:30 +0930

    Bug fix: This override makes the sidebar go on top - no longer needed in spree 3

--- app/overrides/move_sidebar_in_admin_layout.rb
+++
@@ -1,7 +0,0 @@
-Deface::Override.new(
-  virtual_path: 'spree/layouts/admin',
-  name: 'move_sidebar_in_admin_layout',
-  insert_top: 'div#content',
-  cut: "erb[loud]:contains('sidebar')",
-  original: 'fafc2255d8854598527f4e4e5dd2db66337b8ca3'
-)
